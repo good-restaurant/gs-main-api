cef64f7d0841a4b5d245f2053c6550f3d4ec0a5d
Jihyun Kim <ksk208105@gmail.com>
2025-10-26 01:42:09 +0900

    remove: 기존 예시 세팅 데이터들 모두 삭제 #13

--- src/main/java/com/good_restaurant/repository/GoodRestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.good_restaurant.repository;
-
-import com.good_restaurant.entity.GoodRestaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GoodRestaurantRepository extends JpaRepository<GoodRestaurant, Long> {
-}
