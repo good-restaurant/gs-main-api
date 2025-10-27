db2e5843157fccb82782991501d80ee685c6e36e
choi4624 <choi4624@gmail.com>
2025-10-27 09:45:58 +0900

    CORS 해결

---
+++ src/main/java/com/good_restaurant/config/SpringSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.good_restaurant.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+	
+	private final PasswordEncoder passwordEncoder;
+	
+	public SpringSecurityConfig() {
+		this.passwordEncoder = new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    http
+	            .csrf(csrf -> csrf.disable()) // csrf 해제
+	            .authorizeHttpRequests(auth -> auth
+	                    .anyRequest().permitAll() // Allow all requests without authentication
+	            )
+	            .formLogin(login -> login.disable()) // Disable form login
+	            .httpBasic(basic -> basic.disable()) // Disable HTTP Basic authentication
+	            .logout(logout -> logout.disable()) // Disable logout
+	            .cors(cors -> cors.configurationSource(request -> {
+	                        CorsConfiguration configuration = new CorsConfiguration();
+	                        configuration.setAllowedOrigins(Arrays.asList(
+	                                "http://localhost",
+		                            "https://*.i4624.info"
+	                        )); // Update with your client's origins
+	                        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+	                        configuration.setAllowedHeaders(Arrays.asList("*"));
+	                        configuration.setAllowCredentials(true); // Allow credentials if needed
+	                        return configuration;
+	                    })
+	            )
+	    ;
+	
+	    return http.build();
+    }
+	
+
+
+}
