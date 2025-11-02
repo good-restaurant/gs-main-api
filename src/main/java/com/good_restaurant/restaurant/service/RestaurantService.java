6326d5936fa2cfa41902570e707d53d27e34fc43
Jihyun Kim <ksk208105@gmail.com>
2025-11-02 17:49:12 +0900

    refactor: restaurant의 정보를 restaurant_detail로 분리함에 따라 생성 관련한 서비스 로직 수정 #18

--- src/main/java/com/good_restaurant/restaurant/service/RestaurantService.java
+++ src/main/java/com/good_restaurant/restaurant/service/RestaurantService.java
@@ -1,8 +1,10 @@
 package com.good_restaurant.restaurant.service;
 
 import com.good_restaurant.restaurant.domain.Restaurant;
+import com.good_restaurant.restaurant.domain.RestaurantDetail;
 import com.good_restaurant.restaurant.dto.RestaurantCoordinateResDto;
 import com.good_restaurant.restaurant.dto.RestaurantCreateReqDto;
+import com.good_restaurant.restaurant.repository.RestaurantDetailRepository;
 import com.good_restaurant.restaurant.repository.RestaurantRepository;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,11 +17,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class RestaurantService {
 
 	private final RestaurantRepository restaurantRepository;
+	private final RestaurantDetailRepository restaurantDetailRepository;
 
 	/**
 	 * 전체 음식점 좌표를 랜덤으로 조회합니다.
-	 * @param limit
-	 * @return
+	 * @param limit 조회할 음식점 좌표 제한 개수
+	 * @return 음식점 좌표 리스트
 	 */
 	@Transactional(readOnly = true)
 	public List<RestaurantCoordinateResDto> getEntireRestaurantCoordinates(int limit) {
@@ -39,20 +42,31 @@ public class RestaurantService {
 	}
 
 	/**
+	 * 음식점에 대한 자세한 데이터를 조회합니다.
+	 * @param limit
+	 * @return
+	 */
+
+	/**
 	 * 음식점 데이터를 생성합니다.
-	 * @param dto
+	 * @param dto 음식점 생성 요청 DTO
 	 */
 	@Transactional
 	public void createRestaurantData(RestaurantCreateReqDto dto) {
-		restaurantRepository.save(Restaurant.builder()
-			.restaurantName(dto.getRestaurantName())
-			.address(dto.getAddress())
-			.category(dto.getCategory())
-			.menu(dto.getMenu())
-			.phoneNumber(dto.getPhoneNumber())
-			.lon(dto.getLon())
-			.lat(dto.getLat())
-			.build()
+		Restaurant newRestaurant = restaurantRepository.save(Restaurant.builder()
+				.restaurantName(dto.getRestaurantName())
+				.address(dto.getAddress())
+				.category(dto.getCategory())
+				.lon(dto.getLon())
+				.lat(dto.getLat())
+				.build()
+		);
+
+		restaurantDetailRepository.save(RestaurantDetail.builder()
+				.restaurant(newRestaurant)
+				.menu(dto.getMenu())
+				.phoneNumber(dto.getPhoneNumber())
+				.build()
 		);
 	}
 }
