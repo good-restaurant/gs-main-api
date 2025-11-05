6a773b185e14c6b1142ac32966353fd1c4bd9403
choi4624 <choi4624@gmail.com>
2025-11-05 15:21:57 +0900

    detailRepository 삭제

--- src/main/java/com/good_restaurant/restaurant/repository/RestaurantDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.good_restaurant.restaurant.repository;
-
-import com.good_restaurant.restaurant.domain.RestaurantDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantDetailRepository extends JpaRepository<RestaurantDetail, Long> {
-
-}
