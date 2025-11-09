b22bdb4a90f4b212093f170b4fa7bb74ac81cf32
choi4624 <choi4624@gmail.com>
2025-11-09 14:44:32 +0900

    레스토랑 사진 서비스와 singedURL 바탕으로 한 다운로드 서비스 처리

---
+++ src/main/java/com/good_restaurant/restaurant/service/RestaurantPictureService.java
@@ -0,0 +1,10 @@
+package com.good_restaurant.restaurant.service;
+
+import com.good_restaurant.restaurant.domain.RestaurantPicture;
+
+import java.util.List;
+
+public interface RestaurantPictureService {
+	
+	RestaurantPicture getPicture(Long pictureId);
+}
