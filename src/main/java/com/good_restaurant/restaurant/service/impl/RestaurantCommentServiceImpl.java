3704d9ceb876190c7675e84cbb259298efe4f60f
choi4624 <choi4624@gmail.com>
2025-12-03 17:02:29 +0900

    코멘트에 timeRecord를 묻혀 JPA EntityListener를 통해 업데이트 하도록 설정
    updatedAt은 덤
    
    https://github.com/good-restaurant/gs-main-api/issues/51

--- src/main/java/com/good_restaurant/restaurant/service/impl/RestaurantCommentServiceImpl.java
+++ src/main/java/com/good_restaurant/restaurant/service/impl/RestaurantCommentServiceImpl.java
@@ -55,7 +55,7 @@ public class RestaurantCommentServiceImpl implements RestaurantCommentService, B
 		Pageable sorted = PageRequest.of(
 				pageable.getPageNumber(),
 				pageable.getPageSize(),
-				Sort.by(Sort.Direction.DESC, "createdAt")   // 최신순
+				Sort.by(Sort.Direction.DESC, "timeRecord")   // 최신순
 		);
 		
 		return repository.findAll(sorted);
